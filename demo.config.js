8b90c6d39607e05b552a8868d0aaff6f39929e8a
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-04-04 16:58:13 +0200

    chore: add demo config and script

---
+++ demo.config.js
@@ -0,0 +1,59 @@
+let config = {
+  address: "0.0.0.0",
+  ipWhitelist: [],
+  logLevel: ["INFO", "LOG", "WARN", "ERROR", "DEBUG"],
+  language: "de",
+  locale: "de-DE",
+  timeFormat: 24,
+  units: "metric",
+  modules: [
+    {
+      module: "clock",
+      position: "top_left",
+      config: {
+        displaySeconds: false,
+        showDate: true
+      }
+    },
+    {
+      module: "calendar",
+      position: "top_left",
+      header: "Calendar",
+      config: {
+        broadcastPastEvents: true,
+        calendars: [
+          {
+            name: "Demo",
+            url: "http://localhost:8080/modules/MMM-CalendarExt3Journal/demo-calendar.ics",
+            symbol: "calendar",
+            color: "#4285f4"
+          },
+          {
+            name: "Overlapping",
+            url: "http://localhost:8080/modules/MMM-CalendarExt3Journal/demo-calendar-overlap.ics",
+            symbol: "users",
+            color: "#ea4335"
+          }
+        ]
+      }
+    },
+    {
+      module: "MMM-CalendarExt3Journal",
+      position: "bottom_bar",
+      header: "Journal View",
+      config: {
+        locale: "de-DE",
+        days: 5,
+        beginHour: 7,
+        hourLength: 12,
+        height: "600px",
+        calendarSet: []
+      }
+    }
+  ]
+}
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {
+  module.exports = config
+}
